45c9f896d5d9742c6715697606f4e11b28283c4f
倚窗听雨丿醉无心 <994039348@qq.com>
2023-10-21 17:31:26 +0800

    添加一个 Naive UI 组件示例

---
+++ src/web-components/naiveui-demo/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <n-config-provider :cls-prefix="classPrefix">
+    <n-notification-provider>
+      <GetEnv />
+
+      <div root p-2>
+        <NSpace>
+          <n-tooltip>
+            <template #trigger>
+              <NButton>鼠标移上显示提示</NButton>
+            </template>
+            提示
+          </n-tooltip>
+
+          <NButton @click="notification.success({ content: '通知' })">点击显示通知</NButton>
+        </NSpace>
+      </div>
+    </n-notification-provider>
+  </n-config-provider>
+</template>
+
+<script lang="ts" setup>
+  import type { NotificationApiInjection } from 'naive-ui/es/notification/src/NotificationProvider';
+  import { useNotification } from 'naive-ui';
+
+  const classPrefix = useNaive();
+
+  let notification: NotificationApiInjection;
+  function GetEnv() {
+    notification = useNotification();
+  }
+</script>
+
+<style>
+  @import "@unocss/reset/tailwind.css";
+</style>
---
+++ src/web-components/naiveui-demo/info.ts
@@ -0,0 +1,2 @@
+export const name = 'Naive UI 组件示例';
+export const tag = 'st-naiveui-demo';
